70b59956f2f1442fa3542773685c318a0b4f621f
Rosa Jalinas <prof.jalinashgmail.com>
2023-08-05 09:38:02 -0600

    Se agregaron comentarios al programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿int [] numbers={1,2};
+int factor=10;// capturamos esta variable a continuación:
+var query=numbers.Select(n=>n*factor);
+factor=20;  //Cambiar el valor de la variable capturada
+foreach (var item in query)
+Console.Write($"{item}|");
